5c8cc8938ba87e0f441cae3904e5808f446b23b7
pawelosso <84676111+pawelosso@users.noreply.github.com>
2021-10-20 17:07:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
-const buttonmenu = document.querySelector('.hamburger');
-const phonemenu = document.querySelector('.phone-menu')
 
-buttonmenu.addEventListener('click', event => {
+const buttonmenu = document.querySelector('.hamburger');
+const phonemenu = document.querySelector('.phone-menu');
+const navigation = document.querySelector('nav');
+
+buttonmenu.addEventListener('click', event1 => {
 
 buttonmenu.classList.toggle('hamburger--active');
-phonemenu.classList.toggle('phone-menu--active')
+phonemenu.classList.toggle('phone-menu--active');
 
 });
 
+document.addEventListener('scroll', event2 => {
+var scrollpos = window.scrollY;
+
+if(scrollpos>=95){
+    navigation.classList.add('nav--active');
+}
+else(navigation.classList.remove('nav--active'));
+
+})
