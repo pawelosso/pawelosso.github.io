1e4adf5dbceb623d773d079f0c721d31c8c1188f
pawelosso <84676111+pawelosso@users.noreply.github.com>
2021-11-13 11:50:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-
 const buttonmenu = document.querySelector('.hamburger');
 const phonemenu = document.querySelector('.phone-menu');
 const navigation = document.querySelector('nav');
+const bars = document.querySelector('.bars')
+const logo = document.querySelector('.logo');
+const aboutoffset = document.querySelector('.aboutme').offsetTop;
+
 
 buttonmenu.addEventListener('click', event1 => {
 
@@ -14,9 +17,43 @@ phonemenu.classList.toggle('phone-menu--active');
 document.addEventListener('scroll', event2 => {
 var scrollpos = window.scrollY;
 
-if(scrollpos>=95){
+if(scrollpos>=90){
     navigation.classList.add('nav--active');
+    logo.classList.add('logo--active');
 }
-else(navigation.classList.remove('nav--active'));
-
+else
+{
+    navigation.classList.remove('nav--active');
+    logo.classList.remove('logo--active');
+}
 })
+
+document.addEventListener('scroll', event3 =>{
+
+    var scrollpos = window.scrollY;
+    
+    if(scrollpos>=aboutoffset){
+        logo.classList.add('black');
+        bars.classList.add('black-bar');
+    }
+    else{
+        logo.classList.remove('black')
+        bars.classList.remove('black-bar')
+    }
+        
+})
+
+$('.menu1').hover(function(){
+    $('.bar1').toggleClass('bar--active');
+})
+
+$('.menu2').hover(function(){
+    $('.bar2').toggleClass('bar--active');
+})
+
+$('.menu3').hover(function(){
+    $('.bar3').toggleClass('bar--active');
+})
+
+
+
